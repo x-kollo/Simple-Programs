3d8b5d015a5c431f18656daf6482179ee7bc7fb2
x-kollo <novak.bogdanius@gmail.com>
2020-06-08 02:54:07 +0300

    Menu

--- menu/main.cpp
+++ menu/main.cpp
@@ -2,11 +2,13 @@
 
 int main()
 {
-    menu men("Title", 5);
-    men.AddItem("First item!",1);
-    men.AddItem("Second", 2);
-    men.ShowMenu();
-
+    menu mn("Main menu", 3);
+    mn.setHeadFillSymb('=');
+    mn.AddItem("First item!",1);
+    mn.AddItem("Second", 2);
+    mn.AddItem("Third", 3);
+    mn.setBorders('[',']');
+    mn.ShowMenu();
     system("pause");
     return 0;
 }
--- menu/menu.h
+++ menu/menu.h
@@ -12,6 +12,8 @@ private:
     std::vector<std::string> item_names;
     char h_fill_symb;
     bool isLeft = false;
+    short width = 16;
+    char borders[2] = {'[',']'}; 
 public:
     menu() : title(""), last_item(0), h_fill_symb('-'){};
     menu(std::string Title, std::size_t Last_Item) :  title(Title), last_item(Last_Item), h_fill_symb('-') 
@@ -21,24 +23,37 @@ public:
     };
     ~menu(){};
 
-
     std::string getTitle() { return title; }
     std::size_t getItems() { return last_item; }
     std::vector<std::string> & getItemNames() { return item_names; }
 
     void setHeadFillSymb(const char h_fill_symb) { this->h_fill_symb = h_fill_symb; } 
     void setLeft(bool pos = true) { isLeft = pos; } 
-    void AddItem(std::string name, int pos)
+    void setWidth(int W) { width = W; }
+    void setBorders(const char Left, const char Right)
+    {
+        borders[0] = Left;
+        borders[1] = Right;
+    }
+    void AddItem(std::vector<std::string> & names)
+    {
+        item_names = names;
+    }
+    void AddItem(std::string name, int pos) 
     {
         item_names[pos-1] = name;
     }
 
     void ShowMenu()
     {
-        std::cout << std::setw(title.size()+16) << std::setfill(h_fill_symb) 
-        << this->title << std::setw(title.size()+16) << std::setfill(h_fill_symb) << h_fill_symb << std::endl;
+        std::cout << std::setw(width+title.size()) << std::setfill(h_fill_symb) 
+        << this->title << std::setw(width) << std::setfill(h_fill_symb) << h_fill_symb << std::endl;
 
         for(std::size_t i = 1;i<=last_item;++i)
-            std::cout << "[" << i << "] " << item_names[i-1] << std::endl;
+             std::cout << borders[0] << i << borders[1] << " " << item_names[i-1] << std::endl;
+            
+
+       std::cout << std::setw(width+title.size()) << std::setfill(h_fill_symb) 
+        << h_fill_symb  << std::setw(width) << std::setfill(h_fill_symb) << h_fill_symb << std::endl;
     }
 };
