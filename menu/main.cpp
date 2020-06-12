36b9ac2009f42b60a7b2da3aa3a18c1521b1f95e
x-kollo <novak.bogdanius@gmail.com>
2020-06-12 15:48:21 +0300

    Menu

--- menu/main.cpp
+++ menu/main.cpp
@@ -2,12 +2,12 @@
 
 int main()
 {
-    menu mn("Main menu", 3);
-    mn.setHeadFillSymb('=');
+    menu mn("Rating",14);
+    mn.setHeadFillSymb('~');
     mn.AddItem("First item!",1);
     mn.AddItem("Second", 2);
     mn.AddItem("Third", 3);
-    mn.setBorders('[',']');
+    mn.setBorders('|','|');
     mn.ShowMenu();
     system("pause");
     return 0;
